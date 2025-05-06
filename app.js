ca78d6edbbc4dcd68a7360761cced690d9245715
Gzd Akgn <gozzde0990@gmail.com>
2025-05-06 08:34:55 +0200

    cors update

--- app.js
+++ app.js
@@ -16,12 +16,18 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
 
 // Mongoose
 mongoose.connect(
